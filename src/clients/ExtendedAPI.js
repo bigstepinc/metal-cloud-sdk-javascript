a5e251972374d8a8e7aadc41905cfd01e7725969
BigstepRapid <BigstepRapid@bigstep.com>
2019-10-03 11:40:41 +0000

    rapid generated update

--- src/clients/ExtendedAPI.js
+++ src/clients/ExtendedAPI.js
@@ -20,8 +20,8 @@ class ExtendedAPI extends JSONRPC.Client
 		super(strEndpointURL);
 
 		this.addPlugin(new ClientBase.Plugins.ExceptionFilter(true));
-		this.addPlugin(new ClientBase.Plugins.SerializeParameters());
-		this.addPlugin(new ClientBase.Plugins.DeserializeOutput());
+		// this.addPlugin(new ClientBase.Plugins.SerializeParameters());
+		// this.addPlugin(new ClientBase.Plugins.DeserializeOutput());
 	}
 
 	/**
